71d28d7853bd42315034ab10366c0108d42eef3d
AlinaDenisova <denicov_vasya@mail.ru>
2020-08-16 14:00:03 +0400

    changes

--- source/js/scripts/remove-no-js.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-//удаление с body класса no-js
-(function () {
-  document.querySelector('.no-js').classList.remove('no-js');
-})();
--- source/js/scripts/reviews-section.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-(function () {
-  window.reviewsSlider.add(); // Импортирует настройки для слайдера
-})();
